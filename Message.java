2525c345a323170d4b97b605a4b0324cb3b3b031
Kats <20KatsNi@fps.fmtdom.local>
2019-12-19 10:33:13 -0500

    yooo

--- Message.java
+++ Message.java
@@ -1,4 +1,19 @@
 public class Message{
-
+    //fields
+    private final String plaintext;
+    private final String cyphertext;
+    private boolean isEncrypted;
     
+    //constructors
+    public Message(String plaintext){
+        this.plaintext = plaintext;
+        this.isEncrypted = true;
+    }
+
+    public Message(String cyphertext){
+        this.cyphertext = cyphertext;
+        this.isEncrypted = true;
+    }
+
+
 }
