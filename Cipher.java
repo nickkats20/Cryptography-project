e14a12ed76b06937a50c210472d13d3d35ad1abd
Nick Kats <56523917+nickkats20@users.noreply.github.com>
2019-12-21 20:28:07 -0500

    Add files via upload

--- Cipher.java
+++ Cipher.java
@@ -1,19 +1,51 @@
 public class Cipher {
-    //fields
-    private  int shiftAmount;
+    // fields
+    // private int shiftAmount;
     private Message plaintext;
+    private char[] normAlphaL = new char[26];
 
-    //constructor
-    public Cipher(int shiftAmount, Message plaintext){
-    this.shiftAmount = shiftAmount;
-    this.plaintext = plaintext;
-    }
-    
-    //methods
-    public  void encrypt(int shiftAmount, Message plaintext){
-        
-        
+    // constructor
+    public Cipher(Message plaintext) {
+        // this.shiftAmount = shiftAmount;
+        this.plaintext = plaintext;
+        normalAlpha();
     }
 
+    // methods
 
+    private void normalAlpha() {
+        int index = 0;
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            normAlphaL[index] = letter;
+            index++;
+        }
+    }
+
+    public char[] encrypt(int shiftAmount) {
+        String toEncrypt = new String(plaintext.getMessage());
+        char[] comparator = toEncrypt.toCharArray();
+        int index = 0;
+        while (index < comparator.length) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                if (comparator[index] == ch) {
+                    comparator[index] = (char) (ch + shiftAmount);
+                    index++;
+                }
+            }
+
+        }
+        return comparator;
+
+        // int i = 0;
+        // while (comparator[i] <= comparator.length) {
+        // for (int j = 0; j < normAlphaL.length; j++) {
+        // if (comparator[i] == normAlphaL[j]) {
+        // comparator[i] = normAlphaL[j + shiftAmount];
+        // }
+
+        // }
+        // }
+        // String encrypted = new String(comparator);
+        // return encrypted;
+    }
 }
--- test.java
+++ test.java
@@ -1,7 +1,16 @@
-public class test{
+public class test {
     public static void main(String[] args) {
-        Message m1 = new Message("Hello world", false);
-        System.out.println(m1.getEncryptionStatus());
+        Message m1 = new Message("abc", false);
         System.out.println(m1.getMessage());
+        Cipher c1 = new Cipher(m1);
+        System.out.println(c1.encrypt(3));
+
     }
 }
+/*
+ * take a string, convert it into a char array take the elememt in array at
+ * index 1 (first letter in string to be encrypted) determine what the index of
+ * this element is in the alphabet replace the first element in array with the
+ * alphabet + x element
+ * 
+ */
