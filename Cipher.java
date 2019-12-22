e0ff269fe59c76ddf1bb7b50af54915f015c41a6
Nick Kats <56523917+nickkats20@users.noreply.github.com>
2019-12-21 22:46:54 -0500

    Add files via upload

--- Cipher.java
+++ Cipher.java
@@ -2,50 +2,77 @@ public class Cipher {
     // fields
     // private int shiftAmount;
     private Message plaintext;
-    private char[] normAlphaL = new char[26];
 
     // constructor
     public Cipher(Message plaintext) {
         // this.shiftAmount = shiftAmount;
         this.plaintext = plaintext;
-        normalAlpha();
+        // normalAlpha();
     }
 
     // methods
+    public String encrypt(int shiftAmount) {
+        if (plaintext.getEncryptionStatus() == false) {
+            String toEncrypt = new String(plaintext.getMessage());
+            String encrypted = new String();
+            for (int i = 0; i < toEncrypt.length(); i++) {
+                if (Character.isUpperCase(toEncrypt.charAt(i))) {
+                    if ((toEncrypt.charAt(i) + shiftAmount) > 'Z') {
+                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount - 'Z' + 'A' - 1);
+                        encrypted += ch;
+                    } else {
+                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount);
+                        encrypted += ch;
+                    }
+                }
+                if (Character.isLowerCase(toEncrypt.charAt(i))) {
+                    if ((toEncrypt.charAt(i) + shiftAmount) > 'z') {
+                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount - 'z' + 'a' - 1);
+                        encrypted += ch;
+                    } else {
+                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount);
+                        encrypted += ch;
 
-    private void normalAlpha() {
-        int index = 0;
-        for (char letter = 'a'; letter <= 'z'; letter++) {
-            normAlphaL[index] = letter;
-            index++;
+                    }
+                }
+
+            }
+            return encrypted;
+        } else {
+            System.out.println("This text has already been encrypted");
         }
+        return null;
     }
 
-    public char[] encrypt(int shiftAmount) {
-        String toEncrypt = new String(plaintext.getMessage());
-        char[] comparator = toEncrypt.toCharArray();
-        int index = 0;
-        while (index < comparator.length) {
-            for (char ch = 'a'; ch <= 'z'; ch++) {
-                if (comparator[index] == ch) {
-                    comparator[index] = (char) (ch + shiftAmount);
-                    index++;
+    public String decrypt(int amountShifted){
+        if(plaintext.getEncryptionStatus() == true){
+            String toDecrypt = new String(plaintext.getMessage());
+            String decrypted = new String();
+            for (int i = 0; i < toDecrypt.length(); i++) {
+                if (Character.isUpperCase(toDecrypt.charAt(i))) {
+                    if ((toDecrypt.charAt(i) - amountShifted) < 'A') {
+                        char ch = (char) (toDecrypt.charAt(i) - amountShifted + 'Z' - 'A' + 1);
+                        decrypted += ch;
+                    } else {
+                        char ch = (char) (toDecrypt.charAt(i) - amountShifted);
+                        decrypted += ch;
+                    }
+                }
+                if (Character.isLowerCase(toDecrypt.charAt(i))) {
+                    if ((toDecrypt.charAt(i) - amountShifted) < 'a') {
+                        char ch = (char) (toDecrypt.charAt(i) - amountShifted + 'z' - 'a' + 1);
+                        decrypted += ch;
+                    } else {
+                        char ch = (char) (toDecrypt.charAt(i) - amountShifted);
+                        decrypted += ch;
+
+                    }
                 }
             }
-
+            return decrypted;
+        }else{
+            System.out.println("This text is not encrypted");
         }
-        return comparator;
-
-        // int i = 0;
-        // while (comparator[i] <= comparator.length) {
-        // for (int j = 0; j < normAlphaL.length; j++) {
-        // if (comparator[i] == normAlphaL[j]) {
-        // comparator[i] = normAlphaL[j + shiftAmount];
-        // }
-
-        // }
-        // }
-        // String encrypted = new String(comparator);
-        // return encrypted;
+        return null;
     }
 }
--- test.java
+++ test.java
@@ -1,16 +1,8 @@
 public class test {
     public static void main(String[] args) {
-        Message m1 = new Message("abc", false);
+        Message m1 = new Message("Weet cehdydw vqc", true);
         System.out.println(m1.getMessage());
         Cipher c1 = new Cipher(m1);
-        System.out.println(c1.encrypt(3));
-
+        System.out.println(c1.decrypt(16));  
     }
 }
-/*
- * take a string, convert it into a char array take the elememt in array at
- * index 1 (first letter in string to be encrypted) determine what the index of
- * this element is in the alphabet replace the first element in array with the
- * alphabet + x element
- * 
- */
