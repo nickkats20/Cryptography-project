ffc5bd40c344bf1a3b71f8790df775970fbc254a
Nick Kats <nkats2002@gmail.com>
2019-12-26 20:36:14 -0500

    12/26

--- Cipher.java
+++ Cipher.java
@@ -1,78 +1,156 @@
+import java.util.Arrays;
+
 public class Cipher {
     // fields
-    // private int shiftAmount;
     private Message plaintext;
+    private char[] plainLettersL;
+    private char[] plainLettersU;;
+    private String encrypted;
+    private String decrypted;
 
-    // constructor
+    // constructors
     public Cipher(Message plaintext) {
-        // this.shiftAmount = shiftAmount;
         this.plaintext = plaintext;
-        // normalAlpha();
+        encrypted = "";
+        decrypted = "";
+        plainLettersL = this.letterizeL();
+        plainLettersU = this.letterizeU();
     }
 
     // methods
+    private char[] letterizeL() {
+        String plainAlphaL = "abcdefghijklmnopqrstuvwxyz";
+        char[] plainLettersL = plainAlphaL.toCharArray();
+        return plainLettersL;
+    }
+
+    private char[] letterizeU() {
+        String plainAlphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] plainLettersU = plainAlphaU.toCharArray();
+        return plainLettersU;
+
+    }
+
     public String encrypt(int shiftAmount) {
         if (plaintext.getEncryptionStatus() == false) {
             String toEncrypt = new String(plaintext.getMessage());
-            String encrypted = new String();
             for (int i = 0; i < toEncrypt.length(); i++) {
-                if (Character.isUpperCase(toEncrypt.charAt(i))) {
-                    if ((toEncrypt.charAt(i) + shiftAmount) > 'Z') {
-                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount - 'Z' + 'A' - 1);
-                        encrypted += ch;
-                    } else {
-                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount);
-                        encrypted += ch;
-                    }
-                }
                 if (Character.isLowerCase(toEncrypt.charAt(i))) {
-                    if ((toEncrypt.charAt(i) + shiftAmount) > 'z') {
-                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount - 'z' + 'a' - 1);
-                        encrypted += ch;
-                    } else {
-                        char ch = (char) (toEncrypt.charAt(i) + shiftAmount);
-                        encrypted += ch;
+                    int index = Arrays.binarySearch(plainLettersL, toEncrypt.charAt(i));
+                    index += shiftAmount;
+                    if (index > 25) {
+                        index -= 26;
                     }
+                    encrypted += plainLettersL[index];
+                } else if (Character.isUpperCase(toEncrypt.charAt(i))) {
+                    int index = Arrays.binarySearch(plainLettersU, toEncrypt.charAt(i));
+                    index += shiftAmount;
+                    if (index > 25) {
+                        index -= 26;
+                    }
+                    encrypted += plainLettersU[index];
                 }
-
             }
-            plaintext.setEncryptionStatus(true);
-            return encrypted;
         } else {
             System.out.println("This text has already been encrypted");
         }
-        return null;
+        plaintext.setEncryptionStatus(true);
+        return encrypted;
+
     }
 
-    public String decrypt(int amountShifted){
-        if(plaintext.getEncryptionStatus() == true){
+    public String decrypt(int amountShifted) {
+        if (plaintext.getEncryptionStatus() == true) {
             String toDecrypt = new String(plaintext.getMessage());
-            String decrypted = new String();
             for (int i = 0; i < toDecrypt.length(); i++) {
-                if (Character.isUpperCase(toDecrypt.charAt(i))) {
-                    if ((toDecrypt.charAt(i) - amountShifted) < 'A') {
-                        char ch = (char) (toDecrypt.charAt(i) - amountShifted + 'Z' - 'A' + 1);
-                        decrypted += ch;
-                    } else {
-                        char ch = (char) (toDecrypt.charAt(i) - amountShifted);
-                        decrypted += ch;
-                    }
-                }
                 if (Character.isLowerCase(toDecrypt.charAt(i))) {
-                    if ((toDecrypt.charAt(i) - amountShifted) < 'a') {
-                        char ch = (char) (toDecrypt.charAt(i) - amountShifted + 'z' - 'a' + 1);
-                        decrypted += ch;
-                    } else {
-                        char ch = (char) (toDecrypt.charAt(i) - amountShifted);
-                        decrypted += ch;
+                    int index = Arrays.binarySearch(plainLettersL, toDecrypt.charAt(i));
+                    index -= amountShifted;
+                    if (index < 0) {
+                        index += 26;
                     }
+                    decrypted += plainLettersL[index];
+                } else if (Character.isUpperCase(toDecrypt.charAt(i))) {
+                    int index = Arrays.binarySearch(plainLettersU, toDecrypt.charAt(i));
+                    index -= amountShifted;
+                    if (index < 0) {
+                        index += 26;
+                    }
+                    decrypted += plainLettersU[index];
+
                 }
             }
-            plaintext.setEncryptionStatus(false);
-            return decrypted;
-        }else{
+        } else {
             System.out.println("This text is not encrypted");
+
         }
-        return null;
+        plaintext.setEncryptionStatus(false);
+        return decrypted;
+
     }
+
+    // public String encrypt(int shiftAmount) {
+    // if (plaintext.getEncryptionStatus() == false) {
+    // String toEncrypt = new String(plaintext.getMessage());
+    // String encrypted = new String();
+    // for (int i = 0; i < toEncrypt.length(); i++) {
+    // if (Character.isUpperCase(toEncrypt.charAt(i))) {
+    // if ((toEncrypt.charAt(i) + shiftAmount) > 'Z') {
+    // char ch = (char) (toEncrypt.charAt(i) + shiftAmount - 'Z' + 'A' - 1);
+    // encrypted += ch;
+    // } else {
+    // char ch = (char) (toEncrypt.charAt(i) + shiftAmount);
+    // encrypted += ch;
+    // }
+    // }
+    // if (Character.isLowerCase(toEncrypt.charAt(i))) {
+    // if ((toEncrypt.charAt(i) + shiftAmount) > 'z') {
+    // char ch = (char) (toEncrypt.charAt(i) + shiftAmount - 'z' + 'a' - 1);
+    // encrypted += ch;
+    // } else {
+    // char ch = (char) (toEncrypt.charAt(i) + shiftAmount);
+    // encrypted += ch;
+    // }
+    // }
+
+    // }
+    // plaintext.setEncryptionStatus(true);
+    // return encrypted;
+    // } else {
+    // System.out.println("This text has already been encrypted");
+    // }
+    // return null;
+    // }
+
+    // public String decrypt(int amountShifted){
+    // if(plaintext.getEncryptionStatus() == true){
+    // String toDecrypt = new String(plaintext.getMessage());
+    // String decrypted = new String();
+    // for (int i = 0; i < toDecrypt.length(); i++) {
+    // if (Character.isUpperCase(toDecrypt.charAt(i))) {
+    // if ((toDecrypt.charAt(i) - amountShifted) < 'A') {
+    // char ch = (char) (toDecrypt.charAt(i) - amountShifted + 'Z' - 'A' + 1);
+    // decrypted += ch;
+    // } else {
+    // char ch = (char) (toDecrypt.charAt(i) - amountShifted);
+    // decrypted += ch;
+    // }
+    // }
+    // if (Character.isLowerCase(toDecrypt.charAt(i))) {
+    // if ((toDecrypt.charAt(i) - amountShifted) < 'a') {
+    // char ch = (char) (toDecrypt.charAt(i) - amountShifted + 'z' - 'a' + 1);
+    // decrypted += ch;
+    // } else {
+    // char ch = (char) (toDecrypt.charAt(i) - amountShifted);
+    // decrypted += ch;
+    // }
+    // }
+    // }
+    // plaintext.setEncryptionStatus(false);
+    // return decrypted;
+    // }else{
+    // System.out.println("This text is not encrypted");
+    // }
+    // return null;
+    // }
 }
--- test.java
+++ test.java
@@ -1,8 +1,10 @@
 public class test {
     public static void main(String[] args) {
-        Message m1 = new Message("Weet cehdydw vqc", true);
+        Message m1 = new Message("abc", false);
         System.out.println(m1.getMessage());
+        System.out.println(m1.getEncryptionStatus()); 
         Cipher c1 = new Cipher(m1);
-        System.out.println(c1.decrypt(16));  
+        System.out.println(c1.encrypt(3)); 
+        System.out.println(m1.getEncryptionStatus()); 
     }
 }
