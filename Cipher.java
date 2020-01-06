2941427406128738cd5dba669321a1f342a1d627
Nick Kats <nkats2002@gmail.com>
2020-01-06 00:42:36 -0500

    1/6

--- Cipher.java
+++ Cipher.java
@@ -1,58 +1,36 @@
-import java.util.Arrays;
-
 public class Cipher {
     // fields
-    private char[] plainLetters;
-    // private char[] plainLettersU;;
+    private String plainLetters;
     private String encrypted;
     private String decrypted;
 
     // constructors
-    public Cipher(){
+    public Cipher() {
         encrypted = "";
         decrypted = "";
-        plainLetters = this.letterize();
-        // plainLettersU = this.letterizeU();
+        plainLetters = "abcdefghijklmnopqrstuvwxyz";
     }
 
-	// methods
-    private char[] letterize() {
-        String plainAlphaL = "abcdefghijklmnopqrstuvwxyz";
-        char[] plainLettersL = plainAlphaL.toCharArray();
-        return plainLettersL;
-    }
-
-    // private char[] letterizeU() {
-    // String plainAlphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // char[] plainLettersU = plainAlphaU.toCharArray();
-    // return plainLettersU;
-
-    // }
-
+    // methods
     public Message encrypt(Message plaintext, int shiftAmount) {
         if (plaintext.getEncryptionStatus() == false) {
             String toEncrypt = new String(plaintext.getMessage());
             toEncrypt.toLowerCase();
             for (int i = 0; i < toEncrypt.length(); i++) {
-                int index = Arrays.binarySearch(plainLetters, toEncrypt.charAt(i));
+                String letter = toEncrypt.substring(i, i + 1);
+                int index = plainLetters.indexOf(letter);
                 index += shiftAmount;
                 if (index > 25) {
                     index -= 26;
                 }
-                encrypted += plainLetters[index];
-                // } else if (Character.isUpperCase(toEncrypt.charAt(i))) {
-                // int index = Arrays.binarySearch(plainLettersU, toEncrypt.charAt(i));
-                // index += shiftAmount;
-                // if (index > 25) {
-                // index -= 26;
-                // }
-                // encrypted += plainLettersU[index];
+                encrypted += plainLetters.substring(index, index + 1);
             }
 
         } else {
             System.out.println("This text has already been encrypted");
         }
         Message encryptedMessage = new Message(encrypted, true);
+        System.out.println(encryptedMessage.getMessage());
         return encryptedMessage;
     }
 
@@ -60,30 +38,19 @@ public class Cipher {
         if (plaintext.getEncryptionStatus() == true) {
             String toDecrypt = new String(plaintext.getMessage());
             for (int i = 0; i < toDecrypt.length(); i++) {
-
-                int index = Arrays.binarySearch(plainLetters, toDecrypt.charAt(i));
+                String letter = toDecrypt.substring(i, i + 1);
+                int index = plainLetters.indexOf(letter);
                 index -= amountShifted;
                 if (index < 0) {
                     index += 26;
                 }
-                decrypted += plainLetters[index];
-                // } else if (Character.isUpperCase(toDecrypt.charAt(i))) {
-                // int index = Arrays.binarySearch(plainLettersU, toDecrypt.charAt(i));
-                // index -= amountShifted;
-                // if (index < 0) {
-                // index += 26;
-                // }
-                // decrypted += plainLettersU[index];
-
+                decrypted += plainLetters.substring(index, index + 1);
             }
         } else {
             System.out.println("This text is not encrypted");
         }
         Message decryptedMessage = new Message(decrypted, false);
+        System.out.println(decryptedMessage.getMessage());
         return decryptedMessage;
-    }
-
-    public String accessMessage(Message decryptedMessage) {
-        return decryptedMessage.getMessage();
     }
 }
--- test.java
+++ test.java
@@ -2,9 +2,8 @@ public class test {
     public static void main(String[] args) {
         Message m1 = new Message("abc", false);
         System.out.println(m1.getMessage());
-        System.out.println(m1.getEncryptionStatus()); 
+        System.out.println(m1.getEncryptionStatus());
         Cipher c1 = new Cipher();
-        System.out.println(c1.encrypt(m1, 3)); 
-        System.out.println(m1.getEncryptionStatus()); 
+        System.out.println(c1.encrypt(m1, 3));
     }
 }
