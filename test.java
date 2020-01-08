963a4445aeec5bb038a527a223bcb290abd86a36
Kats <20KatsNi@fps.fmtdom.local>
2020-01-08 10:24:24 -0500

    1/8.2

--- test.java
+++ test.java
@@ -1,9 +1,19 @@
 public class test {
     public static void main(String[] args) {
-        Message m1 = new Message("hello austin", false);
+        //encryption test
+        Message m1 = new Message("hello everyone", false);
         System.out.println(m1.getMessage());
         System.out.println(m1.getEncryptionStatus());
         Cipher c1 = new Cipher();
         System.out.println(c1.encrypt(m1, 3).getMessage());
+       
+        System.out.println("********************");
+
+        //decryption test
+        Message m2 = new Message("khoor hyhubrqh", true);
+        System.out.println(m2.getMessage());
+        System.out.println(m2.getEncryptionStatus());
+        Cipher c2 = new Cipher();
+        System.out.println(c2.decrypt(m2, 3).getMessage());
     }
 }
